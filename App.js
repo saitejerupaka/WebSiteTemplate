87b5711f27f3b12ba8dd50b55da76e1770c19312
saitejerupaka <serupaka@gmail.com>
2014-12-12 17:46:25 -0600

    First Commit,
    www.saitejerupaka.com

---
+++ App.js
@@ -0,0 +1,30 @@
+(function () {
+	var app = angular.module('app',["ngRoute"]);
+
+	app.config(function($routeProvider,$locationProvider) {
+		$routeProvider
+			.when("/",{
+				templateUrl:"MySelf.html"
+			})
+			.when("/MySelf",{
+				templateUrl: "MySelf.html"
+			})
+			.when("/Degrees",{
+				templateUrl: "MyDegrees.html"
+			})
+			.when("/Experience",{
+				templateUrl: "MyExperience.html"
+			})
+			.when("/Work",{
+				templateUrl: "MyWork.html"
+			})
+			.when("/Recognition",{
+				templateUrl: "MyRecognition.html"
+			})
+			.when("/Certifications",{
+				templateUrl: "Certifications.html"
+			})
+			.otherwise({redirectTo: "/"});
+		
+	});
+}());
